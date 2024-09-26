196f9000aad062a4967c0331e5291ceecb80e219
VERMAMANISHKUMAR <manishkumarverma091@gmail.com>
2024-09-26 13:24:04 +0530

    Speedtest project is completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import SpeedTest from './SpeedTest';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header bg-primary text-white text-center py-3">
+        <h1>Network Speedtest</h1>
       </header>
+      <div className="container my-5">
+        <SpeedTest />
+      </div>
     </div>
   );
 }
